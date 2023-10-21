a5294b588c6de53168982210e01d5074a0ca5f40
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-10-21 15:02:24 +0800

    Create 作業六範例(可以複製).cpp

---
+++ 作業六範例(可以複製).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int ;       //變數設為x
+    int ;       //變數設為y
+    int ;       //變數設為answer
+    
+    //輸入
+    cin;        //輸入到變數x,y
+    
+    //運算
+    
+    answer=x+y;
+    cout<<"x+y="<<answer<<endl;
+    
+    cout<<"x-y="<<answer<<endl;
+    
+    cout<<"x*y="<<answer<<endl;
+    
+    cout<<"x/y="<<answer<<endl;
+    
+    cout<<"x%y="<<answer<<endl;
+}
