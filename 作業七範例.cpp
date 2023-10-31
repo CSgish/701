dfb7d1be5d1e70aa23a5549e06ecfcf6d79c738c
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-10-31 19:22:57 +0800

    Create 作業七範例.cpp

---
+++ 作業七範例.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //設定變數
+    float ;       //設定為chinese         
+    float ;       //設定為english
+    float ;       //設定為math
+    float ;       //設定為science
+    float ;       //設定為history
+    float ;       //設定為geography
+    float ;       //設定為civics
+    float ;       //設定為average
+    
+    //輸入
+    cout<<"輸入國文分數：";
+    cin>>chinese;        
+    cout<<"輸入英文分數：";
+    
+    cout<<"輸入數學分數：";
+    
+    cout<<"輸入自然分數：";
+    
+    cout<<"輸入歷史分數：";
+    
+    cout<<"輸入地理分數：";
+    
+    cout<<"輸入公民分數：";
+    
+    //加權
+    chinese = ;     //chinese*5
+    english = ;     //english*3
+    math = ;        //math*4
+    science = ;     //science*3
+    
+    //計算平均
+    average = ;     //所有變數加起來
+    average = average/(5+3+4+3+1+1+1);
+    cout<<"平均："<<average<<endl;
+    
+}
