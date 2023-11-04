5bf403e508b1469d65c83b2be28bfaf4740bd5a3
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-11-04 14:13:57 +0800

    Create 作業八範例.cpp

---
+++ 作業八範例.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //設定變數
+    int ;       //變數設定為x
+    int ;       //變數設定為y
+    
+    //輸入
+    cin>>;      //輸入到x,y
+    
+    //條件判斷
+    if
+        cout<<"x=y"<<endl;
+    
+    else if
+        cout<<"x<y"<<endl;
+    
+    else
+        cout<<"x>y"<<endl;
+
+}
